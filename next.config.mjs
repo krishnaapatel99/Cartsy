47b0803913ac0697369921c450504a0de7f61797
Krishna Patel <patelkrishu1608@gmail.com>
2025-11-04 17:24:45 +0530

    updated frontend

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["images.unsplash.com"], // Allow Unsplash images
-  },
-  reactStrictMode: true, // Optional but recommended
-  eslint: {
-    ignoreDuringBuilds: true, // Prevent ESLint errors from failing production build
-  },
-};
+const nextConfig = { images: {
+    domains: ["images.unsplash.com"], // allow this domain
+  },};
 
 export default nextConfig;
